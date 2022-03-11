516a2fc204b3df1ca3d637556b7bccb1d5a888a0
lefmanos <lefmanos@protonmail.com>
2022-03-11 20:02:41 +0200

    Fix fonts

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=14"
-	"octicons:style=Medium"
+	"monospace:size=14",
+	"JoyPixels:pixelsize=14:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
